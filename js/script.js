0858920771acea7a6530bd18ed0bb1fb495f86d3
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-30 17:04:57 +0200

    file elaboration 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('JS OK')
+
+const list = ['pane', 'latte', 'uova', 'burro', 'carne', 'pesce', 'olio', 'sale'];
+let i = 0;
+const expense =document.querySelector('.expense')
+
+while(i < list.length){
+    console.log(list[i])
+    i++
+    expense.innerHTML = list
+}
+
